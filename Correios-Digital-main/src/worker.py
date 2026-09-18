ce13f6d7298651793a4785690f39b12f31565723
Vitor Daniel <143666152+VitorDanielRC@users.noreply.github.com>
2026-09-18 19:30:55 -0300

    Update worker.py

--- Correios-Digital-main/src/worker.py
+++ Correios-Digital-main/src/worker.py
@@ -1,8 +1,15 @@
-from workers import wsgi
+from workers import WorkerEntrypoint, wsgi
 
-from postoffice.presentation import criar_app
+_app = None
 
 
-app = criar_app("dados")
+class Default(WorkerEntrypoint):
+    async def fetch(self, request):
+        global _app
 
-Default = wsgi.entrypoint(app)
+        if _app is None:
+            from postoffice.presentation import criar_app
+
+            _app = criar_app("dados")
+
+        return await wsgi.fetch(_app, request, self.env)
